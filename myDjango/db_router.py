c6ceb76b7ed413c6ee596962a969cbd87293b6ca
Highsir <44265840+Highsir@users.noreply.github.com>
2021-12-30 00:53:25 +0800

    ddd

--- myDjango/db_router.py
+++ myDjango/db_router.py
@@ -1,17 +1,17 @@
 from django.conf import settings
 
-
 DATABASE_MAPPING = settings.DATABASE_APPS_MAPPING
+
 
 class dbAppsRouter(object):
 
     def db_for_read(self, model, **hints):
-        if model._meta.app_lable in DATABASE_MAPPING:
+        if model._meta.app_label in DATABASE_MAPPING:
             return DATABASE_MAPPING[model._meta.app_label]
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_lable in DATABASE_MAPPING:
+        if model._meta.app_label in DATABASE_MAPPING:
             return DATABASE_MAPPING[model._meta.app_label]
         return None
 
